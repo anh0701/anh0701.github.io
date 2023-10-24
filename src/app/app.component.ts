f46b91d9a9853f3bc72e06c7ed95a4f21c4758ad
Pham Thi Quynh Anh <quynhanhphamthi01@gmail.com>
2023-10-24 19:36:24 +0700

    add bootstrap and home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+
+const message = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi vel numquam facere qui dicta veniam sapiente hic laudantium quod similique, ad facilis quisquam officia repudiandae! Possimus doloribus dicta autem exercitationem?"
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  message = "";
   constructor(private title: Title){
     title.setTitle('Anh0701\'s portfolio')
   };
+
+  private typing(): void {
+    let count = 0;
+    const id = setInterval(() => {
+      if (this.message.length === message.length) {
+        clearInterval(id);
+      }
+      this.message += message.charAt(count);
+      count += 1;
+    }, 150);
+  }
   
-  
+  ngOnInit(): void {
+    this.typing();
+  }
 }
