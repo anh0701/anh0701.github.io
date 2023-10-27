2f2f3151e1ffccd609e4c28c8363f79c08c7e3da
Pham Thi Quynh Anh <quynhanhphamthi01@gmail.com>
2023-10-27 18:17:07 +0700

    dark mode v1.0.0

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+  ngOnInit(): void {
+    document.getElementById('btnSwitch')!.addEventListener('click',()=>{
+      if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+          document.documentElement.setAttribute('data-bs-theme','light')
+          document.getElementById('sun')!.classList.add('bi-sun')
+          document.getElementById('sun')!.classList.remove('bi-moon-fill')
+        }
+        else {
+          document.documentElement.setAttribute('data-bs-theme','dark')
+          document.getElementById('sun')!.classList.remove('bi-sun')
+          document.getElementById('sun')!.classList.add('bi-moon-fill')
 
+      }
+  })
+  }
 }
