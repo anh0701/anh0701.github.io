0b24a413593c1ec87442ab7c8f7c8003e8fc6467
Pham Thi Quynh Anh <quynhanhphamthi01@gmail.com>
2023-12-29 10:56:24 +0700

    fix menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -6,6 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  isCollapse = false;
+  
+  check(): void{
+    // setTimeout(() => {
+    //   this.isCollapse = false;
+    // }, 50);
+    this.isCollapse = false;
+  }
+
   ngOnInit(): void {
     document.getElementById('btnSwitch')!.addEventListener('click',()=>{
       if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
